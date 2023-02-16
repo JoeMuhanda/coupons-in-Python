30d2d7789c5c85e4a70ffa0ed57bb65cffc33adb
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-02-16 21:38:54 +0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
+from django.contrib import 
 from .models import Coupons
 # Register your models here.
 @admin.register(Coupons)
 class CouponAdmin(admin.ModelAdmin):
-    list_display = ['code', 'valid_from', 'valid_to', 'discount', '
+    list_display = ['code', 'valid_from', 'valid_to', 'discount', 'active']
     list_filter = ['active', 'valid_from', 'valid_to']
     search_fields = ['code']
